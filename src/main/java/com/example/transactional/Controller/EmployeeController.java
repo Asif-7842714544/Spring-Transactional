c3710e922896fa569567242c988d02451613a840
Asif-7842714544 <casif989@gmail.com>
2024-03-10 17:15:18 +0530

    done

--- src/main/java/com/example/transactional/Controller/EmployeeController.java
+++ src/main/java/com/example/transactional/Controller/EmployeeController.java
@@ -2,8 +2,6 @@ package com.example.transactional.Controller;
 
 import com.example.transactional.Service.EmployeeService;
 import com.example.transactional.VO.EmployeeRequestVO;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+  private EmployeeService employeeService;
 
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+  public EmployeeController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
+  }
 
-
-
-    @PostMapping("/employee")
-    public String saveEmployee(@RequestBody EmployeeRequestVO employeeRequest) {
-      return  employeeService.saveEmployee(employeeRequest);
-    }
+  @PostMapping("/employee")
+  public String saveEmployee(@RequestBody EmployeeRequestVO employeeRequest) {
+    return employeeService.saveEmployee(employeeRequest);
+  }
 
 }
